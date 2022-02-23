37ef6d2054f74091e52586ff3b6f212ab5afccf7
Muhammad <muhammadmnnm1@gmail.com>
2022-02-24 01:48:01 +0500

    Initial Commit

---
+++ TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by Muhammad Islamov on 24/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
